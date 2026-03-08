27b9c31e2299da428164bf920315e565a97dbeb5
changwenliang <2077104989@qq.com>
2026-03-08 16:24:36 +0800

    实现ssedemo

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,48 @@
 <template>
-  <router-view />
-  <login-win v-if="showLoginWin"></login-win>
+    <router-view />
+    <login-win v-if="showLoginWin"></login-win>
 </template>
 <script lang="ts" setup>
-import { getTheme, changeTheme } from '@/utils/theme'
+import { getTheme, changeTheme } from '@/utils/theme';
 import { computed, onMounted } from 'vue';
 import LoginWin from '@/components/Login/LoginWin.vue';
 import { useLoginStore } from './store/loginStore';
 import useUserStore from './store/userStore';
 
-const userStore = useUserStore()
+const userStore = useUserStore();
 
-const loginStore = useLoginStore()
+const loginStore = useLoginStore();
 
 const showLoginWin = computed(() => {
-  return loginStore.isShowLoginWin
-})
+    return loginStore.isShowLoginWin;
+});
 
-function initTheme() { // 初始化主题颜色
-  changeTheme(getTheme());
+function initTheme() {
+    // 初始化主题颜色
+    changeTheme(getTheme());
+}
+
+function createSSE() {
+    let content = '';
+    const sse = new EventSource(
+        `${import.meta.env.VITE_HTTP_BASE_URL}/ai/get-deepseek-service`,
+    );
+    sse.onmessage = (e) => {
+        // e.data 是服务器发送的增量文本
+        content += e.data; // 追加文本
+        console.log('content---', content);
+    };
+
+    sse.onerror = (err) => {
+        console.error('SSE 错误:', err);
+        content += '\n[流式连接已断开]';
+        sse.close(); // 关闭连接
+    };
 }
 
 onMounted(() => {
-  initTheme();
-  userStore.getUserInfo();
-
-})
+    initTheme();
+    userStore.getUserInfo();
+    createSSE();
+});
 </script>
